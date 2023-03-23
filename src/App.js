0a9442d7b2ca41ecac879bf656a73fc2fa1614cd
Nawroz Salehi <salehi.nawroz@gmail.com>
2023-03-23 19:30:55 -0400

    Embedded js in jsx

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import * as React from 'react';
 
+function getTitle(title){
+  return title;
+}
 function App(){
   return (
     <div>
-      <h1>Hello React</h1>
+      <h1>Hello {getTitle("Reactjs")}</h1>
+
+      <label htmlFor='search'>Search </label>
+      <input type="text" id="search"></input>
     </div>
   );
 }
