9bb51d510e1a46b0d41b102aee815a142812d6a9
Nawroz Salehi <salehi.nawroz@gmail.com>
2023-05-07 11:54:59 -0400

    lesson 1.6 added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
 import * as React from 'react';
 
-const List= (probs)=>{
-  return (
-    <div>
-      <h3>{probs.title}</h3>
-      <ul>
-        { probs.lists.map(function (item){
-          return (<Item key={item.objectID} item={item} />);
-          }) 
-        }
-      </ul>
-      {/**/}
-    </div>
-  );
-}
+const initialStories =[
+  {
+    title: 'react',
+    url:'url/abc/db',
+    author: 'Ahmad alizada',
+    num_comments: 4,
+    points:4,
+    objectID:1
+  },
+  {
+    title: 'redux',
+    url:'url/abc/db',
+    author: 'Ahmad Nadem',
+    num_comments: 3,
+    points:2,
+    objectID:7
+  },
+  {
+    title: 'vuex',
+    url:'url/abc/db',
+    author: 'Nawroz Salehi',
+    num_comments: 3,
+    points:2,
+    objectID:2
+  }
+]  
 
-const Item = (probs) =>{
-  const item = probs.item;
+
+const List = ({list, onRemoveItem}) => 
+  list.map(item=>(
+    <Item 
+      key={item.objectID}
+      item={item}
+      onRemoveItem = {onRemoveItem}
+    />
+  ));
+
+const Item = ({item, onRemoveItem}) =>{
+  
   return (
     <li>
       <span>
@@ -24,9 +46,15 @@ const Item = (probs) =>{
       </span>     <span>{item.author}</span>&nbsp;&nbsp;
       <span>{item.num_comments}</span>&nbsp;&nbsp;
       <span>{item.points}</span>
+      <span>
+        <button type="button" onClick={()=>onRemoveItem(item)}>
+          Remove
+        </button>
+      </span>
   </li>
   );
 }
+
 
 
 const Search=(props)=>{
@@ -35,8 +63,7 @@ const Search=(props)=>{
   }
   return (
       <>
-        <label htmlFor='search'>Search </label>
-        <input type="text" value={props.searchTerm} id="search4" onChange={handleChange}></input>
+        
         <p>
           search for <strong>{props.searchTerm}</strong>
         </p>
@@ -46,32 +73,7 @@ const Search=(props)=>{
 
 const App = () => {
 
-  const stories =[
-    {
-      title: 'react',
-      url:'url/abc/db',
-      author: 'Ahmad alizada',
-      num_comments: 4,
-      points:4,
-      objectID:1
-    },
-    {
-      title: 'redux',
-      url:'url/abc/db',
-      author: 'Ahmad Nadem',
-      num_comments: 3,
-      points:2,
-      objectID:7
-    },
-    {
-      title: 'vuex',
-      url:'url/abc/db',
-      author: 'Nawroz Salehi',
-      num_comments: 3,
-      points:2,
-      objectID:2
-    }
-  ] 
+  
 
 
   const useSemiPersistentState = (key, initialState) => {
@@ -86,33 +88,16 @@ const App = () => {
 
   const [searchTerm, setSearchTerm] = useSemiPersistentState('search','vue');
 
+  const [stories, setStories] = React.useState(initialStories);
 
-  // const jsLibs =[
-  //   {
-  //     title: 'jQuery',
-  //     url:'url/abc/db',
-  //     author: 'Fayaz Nabi',
-  //     num_comments: 4,
-  //     points:4,
-  //     objectID:1
-  //   },
-  //   {
-  //     title: 'vuejs',
-  //     url:'url/abc/db',
-  //     author: 'Rohan Nido',
-  //     num_comments: 3,
-  //     points:2,
-  //     objectID:7
-  //   },
-  //   {
-  //     title: 'Angular',
-  //     url:'url/abc/db',
-  //     author: 'Google',
-  //     num_comments: 3,
-  //     points:2,
-  //     objectID:2
-  //   }
-  // ] 
+  const handleRemoveStory = item => {
+    const newStories = stories.filter(
+      story => item.objectID !== story.objectID
+    );
+    setStories(newStories);
+  }
+
+  
   const handleSearch = (event) => {
       setSearchTerm(event.target.value);
       console.log(event.target.value); 
@@ -125,13 +110,51 @@ const App = () => {
     return (
       <div>
         <h1>My Hacker Stories</h1>
-        <Search onSearch={handleSearch} searchTerm={searchTerm}/>
+        {/* <Search onSearch={handleSearch} searchTerm={searchTerm}/> */}
+         <InputWithLabel 
+          id="search" 
+          value={searchTerm} 
+          onInputChange={handleSearch}
+          >
+          <strong>Search</strong> {/* Children */}
+          </InputWithLabel>
+
         <hr/>
-        <List lists={searchedStories} title="React eco system" />
+        <List list={searchedStories} title="React eco system" onRemoveItem={handleRemoveStory} />
         {/* <List lists={jsLibs} title="JavaScript Libraries"/> */}
       </div>
     );
 }
+
+const InputWithLabel = ({
+  id,
+  value,
+  type="text",
+  onInputChange,
+  isFocused,
+  Children
+}) => {
+  const inputRef = React.useRef();
+  React.useEffect(()=>{
+    if(isFocused){
+      inputRef.current.focus();
+    }
+  },[isFocused])
+  return (
+  <>
+    <label htmlFor={id}>{Children}</label>
+    &nbsp;
+    <input 
+      ref={inputRef}
+       id={id} 
+      type={type} 
+      value={value} 
+      onChange={onInputChange}
+      autoFocus
+      />
+  </>
+  )
+};
 
 
 export default App;
